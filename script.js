9d9d3d5fea416f1013f6d87b3fde7475704e30ec
Victoria <shevvictoryia@gmail.com>
2024-07-29 23:54:37 +0300

    feat: add questions array

---
+++ script.js
@@ -0,0 +1,31 @@
+let questions = [
+  {
+    prompt: `Choose the correct HTML element for the largest heading:`,
+    options: ["<heading>", "<head>", "<h6>", "<h1>"],
+    answer: "<h1>",
+  },
+
+  {
+    prompt: `Choose the correct HTML element to define emphasized text`,
+    options: ["<i>", "<em>", "<italic>", "<b>"],
+    answer: "<em>",
+  },
+
+  {
+    prompt: `How can you make a numbered list?`,
+    options: ["<li>", "<ul>", "<ol>", "<dl>"],
+    answer: "<ol>",
+  },
+
+  {
+    prompt: `Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?`,
+    options: ["alt", "src", "title", "type"],
+    answer: "alt",
+  },
+
+  {
+    prompt: `Which HTML element is used to specify a header for a document or section?`,
+    options: ["<header>", "<head>", "<heading>", "<top>"],
+    answer: "<header>",
+  },
+];
